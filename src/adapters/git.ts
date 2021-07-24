a9c58ada4445509af9038f18048f93654362a9e4
Diego de Oliveira <dodiego@gmail.com>
2021-07-24 19:42:50 -0300

    adicionei dist ao repositório temporariamente

--- src/adapters/git.ts
+++ src/adapters/git.ts
@@ -3,14 +3,12 @@ import { promisifyChildProcess, spawnChildProcess } from "./child_process"
 import { assertFile } from "./fs"
 
 export async function assertGitInit() {
-  try {
-    await fs.promises.stat(".git")
-  } catch (error) {
+  if (!fs.existsSync(".git")) {
     await promisifyChildProcess(spawnChildProcess("git init"))
   }
 }
 
 export async function assertGitIgnore() {
-  const gitIgnoreContent = ["node_modules", "dist"].join("\n")
+  const gitIgnoreContent = "node_modules"
   await assertFile(".gitignore", gitIgnoreContent)
 }
