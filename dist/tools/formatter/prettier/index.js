dc22777f5ff0dccfb9c9f50a7707d9331350b46c
Diego de Oliveira <dodiego@gmail.com>
2021-07-24 19:46:26 -0300

    dist

---
+++ dist/tools/formatter/prettier/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.configFileName = exports.name = void 0;
+exports.name = "prettier";
+exports.configFileName = ".prettierrc";
---
+++ dist/tools/linter/eslint/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.configFileName = exports.name = void 0;
+exports.name = "eslint";
+exports.configFileName = ".eslintrc";
