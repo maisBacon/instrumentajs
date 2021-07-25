b9eabc737b7e8d40a35e971336c28dad2d0bfbe3
Diego de Oliveira <dodiego@gmail.com>
2021-07-24 22:21:30 -0300

    adicionei estrutura correta e configuração da cli

--- scripts/run-cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const { spawn } = require("child_process")
-const path = require("path")
-const child = spawn("tsc && npx cross-env NODE_PATH=./dist node dist/src/cli.js", {
-  cwd: path.join(__dirname, ".."),
-  shell: true,
-})
-child.stdout.pipe(process.stdout)
-child.stderr.pipe(process.stderr)
